4c6ba257453f3bcf76f8a4483a7dc0199e665c5a
redmorr <19555512+redmorr@users.noreply.github.com>
2021-12-29 16:35:34 +0100

    Make clip duration calculation only when demanded

--- clipstitch/detectoverlap.py
+++ clipstitch/detectoverlap.py
@@ -10,12 +10,16 @@ class Clip:
     def __init__(self, path):
         self.path = path
         self.start_timestamp = os.path.getmtime(path)
-        self.duration = 1  # ffmpeg.get_duration(path) # Should be resolved only when required / lazy
-        self.end_timestamp = self.start_timestamp + self.duration
+        self.duration = 0
+        self.end_timestamp = 0
 
     def __str__(self):
         return "{:<30}  {}  {}".format(self.path.name, datetime.fromtimestamp(self.start_timestamp),
                                        datetime.fromtimestamp(self.end_timestamp))
+
+    def calculate_duration(self):
+        self.duration = ffmpeg.get_duration(self.path)
+        self.end_timestamp = self.start_timestamp + self.duration
 
 
 def color_generator():
@@ -26,6 +30,8 @@ def color_generator():
 
 def display_overlapping_by_date(clips_path):
     clips = [Clip(path.resolve()) for path in sorted(Path(clips_path).iterdir(), key=os.path.getmtime)]
+    for clip in clips:
+        clip.calculate_duration()
     color_cycler = color_generator()
     color = next(color_cycler)
     is_overlapping = False
